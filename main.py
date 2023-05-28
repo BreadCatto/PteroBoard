6ac4607cfd93df9202b0968b7ebc83414ba4f200
Jay Kumar <jaykumar.bano@gmail.com>
2023-05-28 15:28:50 +0530

    fastapi to flask
    moving from fastapi to flask because its better for me.

--- main.py
+++ main.py
@@ -1,32 +1,97 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from colorama import Fore
-import os, json
+from flask import Flask, render_template, request, url_for, redirect, session
+import pymongo
+import bcrypt
+import json
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-with open("config.json")as jsonfile:
+with open("config.json") as jsonfile:
     conf = json.load(jsonfile)
-title = conf["title"]
 
-for file in os.listdir('routes'):
-    if file.endswith('.py') and file != '__init__.py':
-        module_name = file[:-3]
-        module = __import__(f'routes.{module_name}', fromlist=[module_name])
-        router = getattr(module, 'router')
-        app.include_router(router)
+app = Flask(__name__)
+app.secret_key = "dsvcbdbxbbxccvxvxcvxcvxcvxcv"
+client = pymongo.MongoClient(conf["mongodb_url"])
+db = client.get_database('pteroclient')
+records = db.users
 
-templates = Jinja2Templates(directory="templates")
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    message = ''
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+    if request.method == "POST":
+        user = request.form.get("fullname")
+        email = request.form.get("email")
+        
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
+        user_found = records.find_one({"name": user})
+        email_found = records.find_one({"email": email})
+        if user_found:
+            message = 'There already is a user by that name'
+            return render_template('index.html', message=message)
+        if email_found:
+            message = 'This email already exists in database'
+            return render_template('index.html', message=message)
+        if password1 != password2:
+            message = 'Passwords should match!'
+            return render_template('index.html', message=message)
+        else:
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            user_input = {'name': user, 'email': email, 'password': hashed}
+            records.insert_one(user_input)
+            
+            user_data = records.find_one({"email": email})
+            new_email = user_data['email']
+   
+            return render_template('logged_in.html', email=new_email)
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    print(Fore.GREEN + """
-===========================================
-================PTEROCLIENT================
-===========================================""")
-    print(Fore.BLUE + "\n\nGitHub : https://github.com/BreadCatto/PteroClient")
-    print(Fore.BLUE + "Copyright (c) 2023 Jay Kumar \nLicensed Under a MIT License.")
-    import uvicorn
-    uvicorn.run("main:app", host=conf["host"], port=conf["port"], reload=True)
+
+@app.route('/logged_in')
+def logged_in():
+    if "email" in session:
+        email = session["email"]
+        return render_template('logged_in.html', email=email)
+    else:
+        return redirect(url_for("login"))   
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+       
+        email_found = records.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return redirect(url_for('logged_in'))
+            else:
+                if "email" in session:
+                    return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'Email not found'
+            return render_template('login.html', message=message)
+    return render_template('login.html', message=message)
+
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("signout.html")
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
